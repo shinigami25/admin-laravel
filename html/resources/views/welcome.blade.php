d1b200679894121714a14d61b28fc6f7f6476ed0
laura.ramirez <laura.ramirez@SFE-14473.TELEVISATIM.COM>
2017-06-20 12:32:13 -0500

    1

--- html/resources/views/welcome.blade.php
+++ html/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laura
+                    Laravel-test
                 </div>
 
                 <div class="links">
