5af534c565b11e6471bdf921c8e5299db81208d3
laura.ramirez <laura.ramirez@SFE-14473.TELEVISATIM.COM>
2017-06-20 12:14:12 -0500

    2

--- html/resources/views/welcome.blade.php
+++ html/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel commit
+                    Laura
                 </div>
 
                 <div class="links">
